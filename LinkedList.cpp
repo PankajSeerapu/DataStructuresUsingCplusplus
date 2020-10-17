ead61cef10f03819ba7bf725e9e689532e4c2966
pankajSIR <seerapupankaj@gmail.com>
2020-10-17 18:45:46 +0530

    Insert After Function is Added to Insert NewNode after a Node with a Give Data is Found in the Linked List

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -22,10 +22,15 @@ public:
 	{
 		this->head = nullptr;
 	}
-	void Push(int data)
+	/* The Method Append will add the elements at the End of the Linked List */
+	void Append(int data)
 	{
 		Node *NewNode = new Node(data);
 		Node* temp;
+
+		/* check if the head is pointing to Null 
+		* if Head is pointing to Null then point the head to the First Node of Linked List
+		*/
 		if (this->head == nullptr)
 		{ 
 			this->head = NewNode;
@@ -41,6 +46,56 @@ public:
 			temp->next = NewNode;
 		}
 	}
+	/* The Method Push is used to add the elements to the start of the Linked List*/
+	void Push(int data)
+	{
+		Node* NewNode = new Node(data);
+		 
+		if (this->head == nullptr)
+		{
+			head = NewNode; // If head is Null Make New Node as Head,
+		}
+		else
+		{
+			NewNode->next = head; //If Head is not Null then Change the Head to New Node
+			head = NewNode;
+		}
+	}
+	/* The Method InsertAfter is used to insert New Node after a Node with the given NodeData*/
+	void InsertAfter(int NodeData,int data)
+	{
+		Node *prevNode, *nextNode , *NewNode = new Node(data);
+		prevNode = nextNode = this->head;
+		if (this->head == nullptr)
+		{
+			cout << "This Function Cannot be Used when Given List is Empty" << endl;
+			return;
+		}
+		else
+		{
+			while (prevNode != nullptr)
+			{
+				if (prevNode->data == NodeData)
+				{
+					nextNode = prevNode->next;
+					prevNode->next = NewNode;
+					NewNode->next = nextNode;
+					return;
+				}
+				else
+				{
+					prevNode = prevNode->next;
+				}
+
+			}
+			if (prevNode == nullptr)
+			{
+				cout << "Given Data was not found in the Linked List to Insert New Node" << endl;
+			}
+
+		}
+		return;
+	}
 	void Display()
 	{
 		Node* temp = this->head;
@@ -55,8 +110,15 @@ public:
 int main()
 {
 	Linkedlist L1;
-	L1.Push(3);
-	L1.Push(4);
+	L1.InsertAfter(4, 9);
+	L1.Append(3);
+	L1.Append(4);
+	L1.Push(10);
+	L1.Push(12);
+	L1.InsertAfter(10, 8);
+	L1.InsertAfter(12, 7);
+	L1.InsertAfter(4, 9);
+	L1.InsertAfter(14, 11);
 	L1.Display();
 	return 1;
 }
